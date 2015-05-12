f73371825a9f9629af9d377a06dd0710d6183dac
Sam Fiechter <sam.fiechter@gmail.com>
2015-05-12 08:48:40 -0400

    multiple feeds...

--- twitter-notifier.pl
+++ twitter-notifier.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/local/Cellar/perl/5.20.1/bin/perl
 
 use NET::SSL;
 use LWP::UserAgent;
@@ -32,37 +32,65 @@ my $resp= $ua->post("https://api.twitter.com/oauth2/token",( 'Content-Type' => "
                                                              Content => $uri->encode("grant_type=client_credentials")
                     ));
 $resp->is_success || die $resp->request->as_string . "\n---\n" .$resp->content ."\n";
+
 my $tokresp = decode_json $resp->content;
 
-my $sresp =  $ua->get( "https://api.twitter.com/1.1/statuses/user_timeline.json?count=1&screen_name=briefingcom",
-                       ('Authorization' =>  "Bearer " . $tokresp->{'access_token'})
-    );
-$sresp->is_success || die $sresp->request->as_string . "\n---\n" .$sresp->content ."\n";
-# die $sresp->content;		
-my $lastid, $jsresp;
-while ($sresp->is_success){
-    try {
-        $jsresp = decode_json $sresp->content;
-        foreach my $zz (@{$jsresp}) {
+my @names = ('briefingcom','WDEL','zerohedge','WSJbreakingnews',"DRUDGE_REPORT","EIAgov","Chris451mobile");
+my @lastids = map(0, @names);
+my @counts = map(1, @names);
 
-            my $text =  $zz->{'text'};
-            $text =~ s/\$/\\\$/gi;
-            $text =~ s/"//gi;
-            $text =~ s/&amp;/&/gi;
-            my $url = $text;
-	    $url =~ s%.*(?=http://)%%i; 
-	    if( $lastid < $zz->{'id'}) { $lastid = $zz->{'id'}; }
-	    $cmd  = "/usr/local/bin/terminal-notifier -message \"" . $text . "\" -open \"". $url ."\" -title \"Briefing.com\" -appIcon \"http://www.briefing.com/favicon.ico\"";
-	    system $cmd;
+my $nme = 0;
+
+while (1){
+
+    for ($nme=0;$nme < scalar @names; $nme++){
+        my $cmd;
+        my $posts = 0;
+        if (0 < @counts[$nme]){
+            if (0 == @lastids[$nme] ) {
+                $cmd =  "https://api.twitter.com/1.1/statuses/user_timeline.json?count=1&screen_name=" . @names[$nme];
+                #       system "logger \'$cmd\'";
+            } else {
+                $cmd =  "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" . @names[$nme] . "&since_id=" .@lastids[$nme];
+                #   system "logger \'$cmd\'";
+            }
+            my $sresp =  $ua->get( $cmd ,('Authorization' =>  "Bearer " . $tokresp->{'access_token'}));
+            $sresp->is_success || die $sresp->request->as_string . "\n---\n" .$sresp->content ."\n";
+            try {
+                my $jsresp = decode_json $sresp->content;
+                system "logger ". $sresp->content;
+                foreach my $tweets (@{$jsresp}) {
+                    my $url = "";
+                    my $text =  $tweets->{'text'};
+                    $text =~ s/\$/\\\$/gi;
+                    $text =~ s/"//gi;
+                    $text =~ s/&amp;/&/gi;
+                    if ($tweets->{'entities'}->{'urls'}->[0]->{'url'} ne ""){
+                        $url = $tweets->{'entities'}->{'urls'}->[0]->{'url'};
+                    } else {
+                        if ($text =~ m@((http|ftp|https):\/\/(\S+))@gi){
+                            $url = $1;
+                        }
+                    }
+                    if( (1 * @lastids[$nme]) < (1 * $tweets->{'id'} )) { @lastids[$nme] = $tweets->{'id'}; }
+                    $cmd  = "/usr/local/bin/terminal-notifier -message \"" . $text . "\" -open \"". $url ."\" -title \"@"
+                        . @names[$nme]. "\" -appIcon \"http://www.twitter.com/favicon.ico\"";
+                    system $cmd;
+                    sleep 5;
+                }
+                $posts = scalar @{$jsresp};
+            } catch {
+                print "ERROR : " . $_;
+            };
+            if ( $posts < 13 ){
+                sleep ( 61  - ( 5 * $posts));
+            };
+            @counts[$nme] = $posts;
+        } else {
+            @counts[$nme] = 1;	# only skip one!
         }
-    } catch {
-	print "ERROR : " . $_;
-    };
-    sleep 61;
-    $sresp =  $ua->get( "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=briefingcom&since_id=" .$lastid,
-                        ('Authorization' =>  "Bearer " . $tokresp->{'access_token'})
-        );
-    $sresp->is_success || die $sresp->request->as_string . "\n---\n" .$sresp->content ."\n";
+
+    }
 }
 exit;
 # osascript -e 'display notification "Lorem ipsum dolor sit amet" with title "Title"'
